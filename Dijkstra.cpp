b3b0304c2705c0d76c53ecb07d919b6dd76d2548
Milton Melson <77636982+MiltonMelson@users.noreply.github.com>
2023-02-03 04:23:42 +0000

    update dijkstra printTable function

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -1,7 +1,12 @@
 #include "Graph.h"
 #include <queue>
 
-int main(void) {
+
+void printTable(vector<vector<int>> &distanceTable);
+
+int main(int argc, char* argv[]) {
+
+   // preset graph edges and weights
    const vector<Edge> edges = {
       // (x, y, w) —> edge from `x` to `y` having weight `w`
       {0, 1, 6}, {1, 2, 7}, {2, 0, 5}, {2, 1, 4}, {3, 2, 10}, {5, 4, 1}, {4, 5, 3}
@@ -18,18 +23,18 @@ int main(void) {
 
    // Dijkstra starts here !!!
 
+   // distance table for each node
+   vector<vector<int>> distanceTable(n, vector<int>(n, 1e9));
+
    // create a shortest path table for each node in graph 0-5
    for (int node = 0; node < graph.getNodeCount(); node++) {
 
       // min heap to store {distance, node}
       priority_queue<Pair, vector<Pair>, greater<Pair>> pq;
-      // distance vector to update shorted path
-      // set each not to infinity or very large number first
-      vector<int> distanceVec(graph.getNodeCount(), 1e9);
 
       // starting nodes distance is always 0
-      distanceVec[node] = 0;
-      cout << "Current Node ( " << node << " ): ";
+      distanceTable[node][node] = 0;
+
       // distance, node
       pq.push({0, node});
 
@@ -44,16 +49,23 @@ int main(void) {
             int dst = edge.first;
 
             // if new distance is less than currently stored distance to this node
-            if (dis + w < distanceVec[dst]) {
-               distanceVec[dst] = dis + w;
+            if (dis + w < distanceTable[node][dst]) {
+               distanceTable[node][dst] = dis + w;
                pq.push({w, dst});
             }
          }
       }
-      for (int i = 0; i < distanceVec.size(); i++) {
-         cout << distanceVec[i] << " ";
+   }
+   return 0;
+}
+
+void printTable(vector<vector<int>> &distanceTable) {
+   cout << "\nDistance Table:\n" << endl;
+   for (int i = 0; i < distanceTable.size(); i++) {
+      cout << "Node " << i << ":  ";
+      for (int j = 0; j < distanceTable[0].size(); j++) {
+         cout << distanceTable[i][j] << " ";
       }
       cout << endl;
    }
-   return 0;
 }
